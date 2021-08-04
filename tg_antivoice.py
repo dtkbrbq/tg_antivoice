f931abf2e9ea39617fc9a3179b46df18aad0409d
dtkbrbq <yuki_man@mail.ru>
2021-08-04 19:39:42 +0300

    Update tg_antivoice.py

--- tg_antivoice.py
+++ tg_antivoice.py
@@ -1,7 +1,6 @@
 from telethon import TelegramClient, events, sync
 import logging
 
-
 # These example values won't work. You must get your own api_id and
 # api_hash from https://my.telegram.org, under API Development.
 api_id = 1111111
